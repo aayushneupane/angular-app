49d13fcd04de5b513e2698d10a576e49fe771659
Aayush Neupane <aayush_Alex@yahoo.com>
2018-03-25 01:27:37 -0700

    Server tracker
    Added a button with property binding to add a new server

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,6 +6,11 @@ import {Component} from '@angular/core';
   styleUrls: ['./server.component.css']
 })
 
-export class ServerComponent{
+export class ServerComponent {
+  serverId = 10;
+  serverStatus = 'offline';
 
+  getServerStatus() {
+    return this.serverStatus;
+  }
 }
