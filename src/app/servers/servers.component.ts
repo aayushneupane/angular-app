62060df047873baa889d0f8eb7cd29d63d037900
Aayush Neupane <aayush_Alex@yahoo.com>
2018-03-25 17:21:35 -0700

    two way data binding for server name
    using $event on input text to two two way data binding. also has examples for property binding and string interpolation

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
 
   allowNewServer = false;
+  serverCreationStatus = 'No Server was created';
+  serverName = 'Server1';
 
   constructor() {
     setTimeout(() => {
@@ -17,5 +19,16 @@ export class ServersComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created';
+    console.log('oncreateserver');
+  }
+
+  onUpdateServerName(event: any) {
+    console.log(event);
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
 
 }
